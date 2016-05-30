bb51d650613b8be0a5c0ec295e8ae232b6a48806
Chase McCarthy <chase@code0100fun.com>
2016-05-30 12:36:57 -0400

    Add authorization to allowed CORS headers

--- lib/ember_weekend_api/endpoint.ex
+++ lib/ember_weekend_api/endpoint.ex
@@ -36,6 +36,6 @@ defmodule EmberWeekendApi.Endpoint do
     key: "_ember_weekend_api_key",
     signing_salt: "Z63alZTm"
 
-  plug Corsica, origins: "*", allow_headers: ["accept", "content-type"]
+  plug Corsica, origins: "*", allow_headers: ["accept", "content-type", "authorization"]
   plug EmberWeekendApi.Router
 end

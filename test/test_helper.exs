a6dd03a4ba2c7f921cb151c61826db41a3b08f1b
Gal Tsubery <gal.tsubery@gmail.com>
2017-04-09 11:02:55 -0500

    Remove unnecessary test setup

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,7 +1,3 @@
 {:ok, _} = Application.ensure_all_started(:ex_machina)
 ExUnit.start
 
-Mix.Task.run "ecto.create", ~w(-r EmberWeekendApi.Repo --quiet)
-Mix.Task.run "ecto.migrate", ~w(-r EmberWeekendApi.Repo --quiet)
-Ecto.Adapters.SQL.Sandbox.mode(EmberWeekendApi.Repo, :manual)
-

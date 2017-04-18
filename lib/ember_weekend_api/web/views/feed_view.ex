f590133e60e6ef27029190b05e0873fae6e97db0
Chase McCarthy <chase@code0100fun.com>
2017-04-18 10:22:17 -0500

    Use email address for feed.xml author

--- lib/ember_weekend_api/web/views/feed_view.ex
+++ lib/ember_weekend_api/web/views/feed_view.ex
@@ -5,10 +5,18 @@ defmodule EmberWeekendApi.Web.FeedView do
     conn.assigns.episodes
   end
 
+  defp rfc_2822_date(date) do
+    Timex.Format.DateTime.Formatter.format(date, "%a, %d %b %Y %H:%M:%S %z", :strftime)
+  end
+
   def release_date(episode) do
-    {:ok, date} = episode.release_date
-      |> Timex.Format.DateTime.Formatter.format("%a, %d %b %Y %H:%M:%S %z", :strftime)
+    {:ok, date} = episode.release_date |> rfc_2822_date
     date
+  end
+
+  def pub_date(episodes) do
+    [most_recent | _] = episodes
+    release_date(most_recent)
   end
 
   def show_notes(model) do
